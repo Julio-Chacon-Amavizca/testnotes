73a0bbc63e440e17b9d432af59deca1b30776b89
JulioChacon <jcchacon3000@gmail.com>
2024-03-16 19:24:14 -0700

    Se añade componente de notas

---
+++ src/components/Note.js
@@ -0,0 +1,16 @@
+
+import '../styles/Note.css';
+
+export const Note = ({ note, toggleImportance }) => {
+    const label = note.important
+        ? 'make not important'
+        : 'make important';
+
+    return (
+        <li className='note'>
+            <p className='showNotes'>{note.content}</p>
+            <button onClick={toggleImportance}>{label}</button>
+        </li>
+    )
+}
+
