65c81bb3340b3c182f386d8000c5efa972c4e7f1
JulioChacon <jcchacon3000@gmail.com>
2024-03-16 19:27:43 -0700

    Se añade servicio para notas. Create, getAll, update.

---
+++ src/services/notes.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+const urlBase = "http://localhost:3001/api/notes";
+
+let token = null;
+
+const setToken = newToken => {
+    token = `bearer ${newToken}`;
+}
+
+const create = (newObject) => {
+    const config = {
+        headers: { Authorization: token }
+    }
+    const request = axios.post(urlBase, newObject, config);
+    return request.then(response => response.data);
+}
+
+const getAll = () => {
+    const request = axios.get(urlBase);
+    return request.then(response => response.data);
+}
+
+const update = (id, newObject) => {
+    const config = {
+        headers: { Authorization: token }
+    }
+    const request = axios.put(`${urlBase}/${id}`, newObject, config);
+    return request.then(response => response.data);
+}
+
+const notesService = { create, getAll, update, setToken };
+
+export default notesService;
