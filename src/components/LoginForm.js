4493c7dced7e75a1cb357911e32023792b11ee61
JulioChacon <jcchacon3000@gmail.com>
2024-03-18 17:45:48 -0700

    Se trabaja con proptypes y se envuelve componente en Togglable

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,45 +1,43 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Togglable from './Togglable';
+import PropTypes from 'prop-types';
 
 
 export default function LoginForm({ handleSubmit, ...props }) {
-    const [loginvisible, setLoginVisible] = useState(false)
-    const hideWhenVisible = { display: loginvisible ? 'none' : '' }
-    const showWhenVisible = { display: loginvisible ? '' : 'none' }
-
 
     return (
-        <div>
-
-            <div className='loginForm' style={hideWhenVisible}>
-                <button onClick={() => setLoginVisible(true)}> Show loggin</button>
-            </div>
-
-            <div className='loginForm' style={showWhenVisible}>
-                <form className='loginForm' onSubmit={handleSubmit}>
-                    <div>
-                        <input
-                            className='username-input'
-                            type='text'
-                            value={props.username}
-                            name='username'
-                            onChange={props.handleUsernameChange}
-                            placeholder="Usuario"
-                        />
-                    </div>
-                    <div>
-                        <input
-                            className='password-input'
-                            type='password'
-                            value={props.password}
-                            name='password'
-                            onChange={props.handlePasswordChange}
-                            placeholder="Contraseña"
-                        />
-                    </div>
-                    <button type='submit' className='send'>Login</button>
-                </form>
-                <button onClick={() => setLoginVisible(false)}>Cancel</button>
-            </div>
-        </div>
+        <Togglable buttonLabel='Show Login'>
+            <form className='loginForm' onSubmit={handleSubmit}>
+                <div>
+                    <input
+                        className='username-input'
+                        type='text'
+                        value={props.username}
+                        name='username'
+                        onChange={props.handleUsernameChange}
+                        placeholder="Usuario"
+                    />
+                </div>
+                <div>
+                    <input
+                        className='password-input'
+                        type='password'
+                        value={props.password}
+                        name='password'
+                        onChange={props.handlePasswordChange}
+                        placeholder="Contraseña"
+                    />
+                </div>
+                <button type='submit' className='send'>Login</button>
+            </form>
+        </Togglable>
     )
+}
+
+LoginForm.propTypes = {
+    handleSubmit: PropTypes.func.isRequired,
+    handleUsernameChange: PropTypes.func.isRequired,
+    handlePasswordChange: PropTypes.func.isRequired,
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired
 }
