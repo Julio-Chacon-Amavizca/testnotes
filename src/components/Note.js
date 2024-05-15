e2337ed95923c8a836601536ffcd37abd218c65d
JulioChacon <jcchacon3000@gmail.com>
2024-05-14 17:49:15 -0700

    Cambios en estilos css.

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,21 +3,20 @@ import '../styles/Note.css';
 import { Link } from 'react-router-dom';
 
 export const Note = ({ note, toggleImportance }) => {
+
     const label = note.important
         ? 'make not important'
         : 'make important';
 
     return (
         <>
-            <div className='note'>
-                <td>
-                    <Link to={`/notes/${note.id}`} className='showNotes'>{note.content}</Link>
-                </td>
-                <td>
-                    <Button onClick={toggleImportance}>{label}</Button>
-                </td>
-                {/* {note.content} */}
-            </div>
+            <td>
+                <Link to={`/notes/${note.id}`} className='showNotes'>{note.content}</Link>
+            </td>
+            <td>
+                <Button onClick={toggleImportance}>{label}</Button>
+            </td>
+            {/* {note.content} */}
         </>
     )
 }
