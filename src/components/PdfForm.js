b3ecc885c59f9ea65157be6a54c7bcddc46aca79
JulioChacon <jcchacon3000@gmail.com>
2024-05-22 03:35:59 -0700

    Se cambia diseño. Se agrega funcionalidad para enviar archivos a la api.

--- src/components/PdfForm.js
+++ src/components/PdfForm.js
@@ -1,16 +1,19 @@
 import { useState } from "react"
 import "./PdfStyles.css"
 
-const containerClasses = "p-4 max-w-lg mx-auto";
-const labelClasses = "block text-sm font-medium text-zinc-700 dark:text-gray-950";
-const inputTextClasses = "mt-1 block w-full px-3 py-2 bg-white border border-zinc-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 dark:bg-zinc-800 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white";
-const buttonClasses = "mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50";
-
-
-export default function PdfForm() {
+export default function PdfForm({ addThePDF }) {
     const [title, setTitle] = useState('');
     const [file, setFile] = useState(null);
 
+    /*
+    const [allImages, setAllImages] = useState(null);
+    /*
+    /*
+        useEffect(() => {
+            getPdf();
+        }, []);
+    
+      */
     const handleTitleChange = (event) => {
         setTitle(event.target.value);
     };
@@ -18,38 +21,141 @@ export default function PdfForm() {
     const handleFileChange = (event) => {
         setFile(event.target.files[0]);
     };
+    /*
+        const getPdf = async () => {
+            const result = await axios.get('http://localhost:3001/api/get-files');
+    
+            setAllImages(result.data.data);
+        };
+    */
 
-    const handleSubmit = (event) => {
+    const submitImage = (event) => {
         event.preventDefault();
-        // Process the form data here, e.g., send to a backend server or log to console
-        console.log('Title:', title);
-        console.log('File:', file);
-    };
+        const pdfObject = new FormData();
+        pdfObject.append('title', title);
+        pdfObject.append('file', file);
+
+        if (title !== null && file !== null) {
+            addThePDF(pdfObject)
+            setFile(null);
+            setTitle('');
+            pdfObject.delete('title');
+            pdfObject.delete('file');
+        } else {
+            alert('Por favor, seleccione un nuevo archivo PDF');
+        }
+
+
+
+
+        /*if (pdfObject.has('title') && pdfObject.has('file')) {
+            console.log('Archivo subido correctamente');
+        } else {
+            console.log('Error al subir el archivo');
+        }*/
+
+
+
+        /*
+        const newObject = {
+            title: title,
+            file: file,
+        };
+        addThePDF(newObject);
+        */
+
+        /*const result = await axios.post(
+            "http://localhost:3001/api/PdfUpload",
+            formData,
+            {
+                headers: { "Content-Type": "multipart/form-data" },
+            }
+        );*/
+
+        //console.log(newObject);
+    }
+
+
 
     return (
-        <div className={containerClasses}>
-            <h2 className="text-2xl font-bold text-zinc-800 dark:text-white mb-4">Cargar Archivo PDF</h2>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="title" className={labelClasses}>Título del documento</label>
-                <input
-                    type="text"
-                    id="title"
-                    name="title"
-                    className={inputTextClasses}
-                    placeholder="Ingrese un título"
-                    value={title}
-                    onChange={handleTitleChange}
-                />
-                <label htmlFor="file-upload" className={`${labelClasses} mt-4`}>Subir archivo PDF</label>
-                <input
-                    type="file"
-                    id="file-upload"
-                    name="file-upload"
-                    className="mt-1 block w-full text-sm text-zinc-900 bg-zinc-50 rounded-lg border border-zinc-300 cursor-pointer dark:text-zinc-400 dark:bg-zinc-700 dark:border-zinc-600 file:mr-4 file:py-2 file:px-4 file:rounded-lg file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
-                    onChange={handleFileChange}
-                />
-                <button type="submit" className={buttonClasses}>Enviar</button>
-            </form>
+        <div className="flex-1 flex flex-col items-center justify-start p-10 bg-zinc-100">
+            <div className="w-full max-w-lg mb-8">
+                <form key={'a'} className="bg-white shadow-md rounded px-8 pt-6 pb-8" onSubmit={submitImage}>
+                    <div className="mb-4">
+                        <label className="block text-zinc-700 text-sm font-bold mb-2" htmlFor="title">
+                            Title
+                        </label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 leading-tight focus:outline-none focus:shadow-outline"
+                            type="text"
+                            id="title"
+                            name="title"
+                            placeholder="Ingrese un título"
+                            value={title}
+                            onChange={handleTitleChange}
+                            required />
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-zinc-700 text-sm font-bold mb-2" htmlFor="file-upload">
+                            Upload PDF
+                        </label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 leading-tight focus:outline-none focus:shadow-outline"
+                            type="file"
+                            id="file-upload"
+                            name="file-upload"
+                            accept="application/pdf"
+                            onChange={handleFileChange}
+                            required />
+                    </div>
+                    <div className="flex justify-center">
+                        <button
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                            Upload File
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
+
+/*
+<div className={containerClasses}>
+<h2 className="text-2xl font-bold text-zinc-800 dark:text-white mb-4">Cargar Archivo PDF</h2>
+<form onSubmit={submitImage}>
+    <label htmlFor="title" className={labelClasses}>Título del documento</label>
+    <input
+        type="text"
+        id="title"
+        name="title"
+        className={inputTextClasses}
+        placeholder="Ingrese un título"
+        value={title}
+        onChange={handleTitleChange}
+        required
+    />
+    <label htmlFor="file-upload" className={`${labelClasses} mt-4`}>Subir archivo PDF</label>
+    <input
+        type="file"
+        id="file-upload"
+        name="file-upload"
+        className="mt-1 block w-full text-sm text-zinc-900 bg-zinc-50 rounded-lg border border-zinc-300 cursor-pointer dark:text-zinc-400 dark:bg-zinc-700 dark:border-zinc-600 file:mr-4 file:py-2 file:px-4 file:rounded-lg file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
+        onChange={handleFileChange}
+        required
+    />
+    <div className="flex justify-center">
+        <button type="submit" className={buttonClasses}>Enviar</button>
+    </div>
+</form>
+<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+    {allImages == null
+        ? ""
+        : allImages.map(item => (
+            <div className={cardContainerClass} key={item.id}>
+                <img className="w-16 h-16 mb-4" src={item.imageUrl} alt="PDF Icon" />
+                <h3 className="text-lg font-semibold dark:text-white">{item.title}</h3>
+                <p className={`${textColorClass} mb-4`}>{item.description}</p>
+                <button onClick={() => showPDF(item.pdf)} className={buttonClass}>Abrir PDF</button>
+            </div>
+        ))}
+</div>
+</div>*/
