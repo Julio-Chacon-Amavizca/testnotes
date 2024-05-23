eb74d5a5a2113e9bb01201ce6f23c981bada4d9f
JulioChacon <jcchacon3000@gmail.com>
2024-05-22 22:41:44 -0700

    Se añade funcionalidad de eliminar archivo.

--- src/Pdf.js
+++ src/Pdf.js
@@ -6,7 +6,7 @@ import { Pdf } from "./components/Pdf";
 import PdfForm from "./components/PdfForm";
 */
 const Pdfs = () => {
-    const { pdfs, addPdf } = usePdf()
+    const { pdfs, addPdf, deletePdf } = usePdf()
 
     useEffect(() => {
         console.log('PDF')
@@ -21,17 +21,31 @@ const Pdfs = () => {
                 }
             })
     }
+
+    const eliminarPdf = (id, pdf, title, index) => {
+        var opcion = window.confirm("Estás Seguro que deseas Eliminar el elemento: " + id + " \nCon nombre: " + title);
+        if (opcion === true) {
+            deletePdf(id, index)
+                .catch(error => {
+                    if (!error.response) {
+                        console.log('Error: Network Error' + error)
+                    }
+                })
+        }
+    };
+
+    // const deletePdf = (id) => {
     return (
         <>
-            <PdfForm addThePDF={addOfPdf}/>
+            <PdfForm addThePDF={addOfPdf} />
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-4 p-4 ">
                 {
                     pdfs == null
                         ? ''
-                        : pdfs.map((file, i) => (
-                            <div key={i}>
-                                <Pdf key={file.id} title={file.title} pdf={file.pdf} id={file.id} />
+                        : pdfs.map((file, indexItem) => (
+                            <div key={indexItem}>
+                                <Pdf key={file.id} index={indexItem} eliminarPdf={eliminarPdf} title={file.title} pdf={file.pdf} id={file.id} />
                             </div>
                         ))
                 }
