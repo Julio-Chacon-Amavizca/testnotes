7b14605aea54771b90b9401ec04c10dee6bdbaf1
JulioChacon <jcchacon3000@gmail.com>
2024-05-24 16:07:48 -0700

    Se crean hooks personalizados para usuarios.

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,19 +1,29 @@
-import { useEffect, useContext } from "react"
+import { useEffect, useContext, useState } from "react"
 import noteService from "../services/notes"
 import loginService from "../services/login"
 import Context from "../context/userContextProvider"
 import pdfsService from "../services/pdfs"
+import userService from "../services/user"
 
 export const useUser = () => {
-    const {user, setUser} = useContext(Context)
+    const { user, setUser } = useContext(Context)
+    const [users, setUsers] = useState([])
 
     useEffect(() => {
+        userService
+            .getAll()
+            .then(initialUsers => {
+                console.log(initialUsers)
+                setUsers(initialUsers)
+            })
         const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
         if (loggedUserJSON) {
             const user = JSON.parse(loggedUserJSON)
             setUser(user)
             noteService.setToken(user.token)
             pdfsService.setToken(user.token)
+            userService.setToken(user.token)
+
         }
     }, [setUser])
 
@@ -22,6 +32,7 @@ export const useUser = () => {
         console.log('user', user)
         noteService.setToken(null)
         pdfsService.setToken(null)
+        userService.setToken(null)
         window.localStorage.removeItem('loggedNoteappUser')
     }
 
@@ -34,13 +45,53 @@ export const useUser = () => {
         window.localStorage.setItem('loggedNoteappUser', JSON.stringify(user))
         noteService.setToken(user.token)
         pdfsService.setToken(user.token)
+        userService.setToken(user.token)
 
         setUser(user)
     }
 
+    const create = async (userObject) => {
+        return userService
+            .createUser(userObject)
+            .then(returnedUser => {
+                if (returnedUser.status === 201) {
+                    setUsers(users.concat(returnedUser.data))
+                    alert('Usuario creado con éxito')
+                }
+            })
+    }
+
+    const deletes = (id, index) => {
+        userService
+            .deleteUser(id)
+            .then(data => {
+                if (data.status === 204) {
+                    alert('Usuario eliminado correctamente')
+                    setUsers(users.filter((user, indexItem) => indexItem !== index))
+                }
+            })
+    }
+
+    const editUserHook = (userToEditx) => {
+        console.log('editUserHook: ', userToEditx)
+        userService
+            .updateUser(userToEditx.id, userToEditx)
+            .then(data => {
+                console.log(data)
+                if (data.status === 200) {
+                    alert('Usuario editado correctamente')
+                    setUsers(users.map(user => user.id !== userToEditx.id ? user : userToEditx))
+                }
+            })
+    }
+
     return {
         user,
+        users,
         logout,
-        login
+        login,
+        create,
+        deletes,
+        editUserHook
     }
 }
